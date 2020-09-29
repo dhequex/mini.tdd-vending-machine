0887e012a48f3c4e0405e15b43128f8d49cca500
Jose M. Torres <jmtorresr@gmail.com>
2020-09-29 17:00:09 +0900

    Added Balance, Till, SelectedRow, Selected Column tests and code passing those tests. Insert coin method increasses balance.
    Co-authored-by: dhequex <dhequex@users.noreply.github.com>
    Co-authored-by: alexdang1993374 <alexdang1993374@users.noreply.github.com>

--- VendingMachine.js
+++ VendingMachine.js
@@ -4,3 +4,27 @@
   What is that? Well, glad you asked.
   Read about it here: https://www.sitepoint.com/understanding-module-exports-exports-node-js/
 */
+
+class VendingMachine {
+  constructor() {
+    this.balance = 0;
+    this.till = {
+      oneYen: { value: 1, count: 0 },
+      fiveYen: { value: 5, count: 0 },
+      tenYen: { value: 10, count: 0 },
+      fiftyYen: { value: 50, count: 0 },
+      hundredYen: { value: 100, count: 0 },
+      fiveHundredYen: { value: 500, count: 0 },
+    };
+    this.selectedRow = "A";
+    this.selectedColumn = 1;
+  }
+
+  insertCoin(denomination) {
+    this.balance += denomination.value;
+  }
+}
+
+let _ = new VendingMachine();
+console.log(typeof _.insertCoin);
+module.exports = VendingMachine;
--- test/test.js
+++ test/test.js
@@ -2,6 +2,38 @@ const VendingMachine = require("../VendingMachine");
 const { expect } = require("chai");
 
 describe("vending machine", () => {
+  it("should have an insertCoin Method", () => {
+    let machine = new VendingMachine();
+    expect(typeof machine.insertCoin).to.equal("function");
+  });
+
+  it("should have a Balance variable", () => {
+    let machine = new VendingMachine();
+    expect(typeof machine.balance).to.equal("number");
+  });
+
+  it("should have a till to map coins", () => {
+    let machine = new VendingMachine();
+    expect(typeof machine.till).to.equal("object");
+  });
+
+  it("should have selectedRow storage", () => {
+    let machine = new VendingMachine();
+    expect(typeof machine.selectedRow).to.equal("string");
+  });
+
+  it("should have selectedColumn storage", () => {
+    let machine = new VendingMachine();
+    expect(typeof machine.selectedColumn).to.equal("number");
+  });
+
+  it.only("should modify Balance after inserting coin", () => {
+    let machine = new VendingMachine();
+    let coin = { value: 50 };
+    machine.insertCoin(coin);
+    expect(machine.balance).to.equal(50);
+  });
+
   it("should accept valid coins", () => {
     // Setup
     const machine = new VendingMachine();
